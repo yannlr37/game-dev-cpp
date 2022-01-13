c7927bddbf54468986854c8a82a616c02497e8ba
yannlr37 <yannlarosa@gmail.com>
2022-01-13 22:39:14 +0100

    Version POO

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,18 +2,33 @@
 #include <iostream>
 
 Game::Game() 
+: mWindow(sf::VideoMode(640, 480), "SFML Application")
+, mPlayer()
 {
-
+   	mPlayer.setRadius(40.f);
+   	mPlayer.setPosition(100.f, 100.f);
+   	mPlayer.setFillColor(sf::Color::Blue);
 }
 
 void Game::run() 
 {
-	std::cout << "New Game" << std::endl;
+	std::cout << "Start game ..." << std::endl;
+	while (mWindow.isOpen())
+   	{
+   		processEvents();
+		update();
+		render();	
+   	}
 }
 
 void Game::processEvents() 
 {
-
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+   		if (event.type == sf::Event::Closed)
+       		mWindow.close();
+	}
 }
 
 void Game::update() 
@@ -23,5 +38,7 @@ void Game::update()
 
 void Game::render() 
 {
-
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
 }
